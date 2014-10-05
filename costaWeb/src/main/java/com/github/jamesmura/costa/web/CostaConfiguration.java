ca9cf66b7bc6daa1afb091041edd507b185a24a7
James Muranga <jmured@gmail.com>
2014-10-05 22:40:28 +0300

    setup db config to pick heroku environment variable

--- costaWeb/src/main/java/com/github/jamesmura/costa/web/CostaConfiguration.java
+++ costaWeb/src/main/java/com/github/jamesmura/costa/web/CostaConfiguration.java
@@ -1,12 +1,13 @@
 package com.github.jamesmura.costa.web;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-
-import io.dropwizard.Configuration;
-import io.dropwizard.db.DataSourceFactory;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class CostaConfiguration extends Configuration {
 
@@ -16,6 +17,32 @@ public class CostaConfiguration extends Configuration {
     private DataSourceFactory database = new DataSourceFactory();
 
     public DataSourceFactory getDataSourceFactory() {
-        return database;
+        String database_url = System.getenv("DATABASE_URL");
+        if (database_url == null) {
+            return database;
+        } else {
+            return create(database_url);
+        }
+    }
+
+    public static DataSourceFactory create(String databaseUrl) {
+        if (databaseUrl == null) {
+            throw new IllegalArgumentException("The DATABASE_URL environment variable must be set before running the app " +
+                    "example: DATABASE_URL=\"postgres://jamesmuranga:jamesmuranga@localhost:5432/costa\"");
+        }
+        DataSourceFactory datasourceFactory = null;
+        try {
+            URI dbUri = new URI(databaseUrl);
+            String user = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+            datasourceFactory = new DataSourceFactory();
+            datasourceFactory.setUser(user);
+            datasourceFactory.setPassword(password);
+            datasourceFactory.setUrl(url);
+            datasourceFactory.setDriverClass("org.postgresql.Driver");
+        } catch (URISyntaxException e) {
+        }
+        return datasourceFactory;
     }
 }
